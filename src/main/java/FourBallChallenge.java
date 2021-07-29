09e49682c923f076e70d003ac446d4c299f18a88
KeerthanaR2910 <keerthurkkeerthana@gmail.com>
2021-07-29 16:41:54 +0530

    third commit

--- src/main/java/FourBallChallenge.java
+++ src/main/java/FourBallChallenge.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class FourBallChallenge {
+import processing.core.PApplet;
+
+public class FourBallChallenge extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    private int x=0;
+    int height = HEIGHT / 5;
+
+    public static void main(String[] args) {
+        PApplet.main("FourBallChallenge",args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH,HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void draw() {
+        drawCircle();
+
+    }
+
+    private void drawCircle() {
+        for(int i=1;i<=4;i++) {
+            ellipse(x*i, height*i, 10, 10);
+        }
+        x++;
+    }
+
 }
